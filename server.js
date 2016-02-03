65cd75b898b0cf94150dfcf73898e2b81f9be169
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-02 16:44:53 -0800

    set up Mongolab db

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express  = require('express');
 var morgan  = require('morgan');
+var mongoose = require('mongoose');
 
 var app  = express();
 
+mongoose.connect('mongodb://root:abc123@ds055515.mongolab.com:55515/kentest', function(err){
+  if (err){
+    console.log(err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+// middelware  - shows routes hit
 app.use(morgan('dev'));
 
 app.get('/', function(req, res){
