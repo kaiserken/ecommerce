a8de717bb278539fb7ac627d3e9617d929ef0700
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-02 20:56:00 -0800

    added EJS templating

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express  = require('express');
 var morgan  = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var ejs  = require('ejs');
+var ejsmate  = require('ejs-mate');
+
+var User  = require('./models/user');
 
 var app  = express();
 
@@ -14,15 +19,30 @@ mongoose.connect('mongodb://root:abc123@ds055515.mongolab.com:55515/kentest', fu
 
 // middelware  - shows routes hit
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.engine('ejs', ejsmate);
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  var name  = "Ken";
-  res.json("My name is " + name);
+app.post('/create-user', function(req, res, next){
+  var user  = new User();
+
+  user.profile.name  = req.body.name;
+  user.password  = req.body.password;
+  user.email  = req.body.email;
+
+  user.save(function(err){
+    if (err) return next(err);
+    res.json('successfully created new user');
+  });
 });
 
-app.get('/catname', function(req, res){
-  res.json('Batman');
+app.get('/', function(reg, res){
+  res.render('home');
 });
+
+
+
 
 app.listen(3000, function(err){
   if (err) throw err;
