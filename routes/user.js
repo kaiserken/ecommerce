0ff45420a46d1d23556b48430b9dddd04eb4f351
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-05 10:14:48 -0800

    adding edit profile route and avatar

--- routes/user.js
+++ routes/user.js
@@ -36,6 +36,7 @@ router.post('/signup', function(req, res, next){
   user.profile.name  = req.body.name;
   user.email  = req.body.email;
   user.password  = req.body.password;
+  user.profile.picture = user.gravatar();
 
   User.findOne({ email: req.body.email}, function(err, existingUser){
 
@@ -60,6 +61,24 @@ router.post('/signup', function(req, res, next){
 router.get('/logout', function(req, res, next){
   req.logout();
   res.redirect('/');
+});
+
+router.get('/edit-profile', function(req, res, next){
+  res.render('accounts/edit-profile.ejs', {message : req.flash('success')});
+});
+
+router.post('/edit-profile', function(req, res, next){
+  User.findOne({_id: req.user._id}, function(err, user){
+    if (err) return next(err);
+    if (req.body.name) user.profile.name = req.body.name;
+    if (req.body.address) user.profile.address = req.body.address;
+
+    user.save(function(err){
+      if (err) return next(err);
+      req.flash('success', 'Successfully edited your profile');
+      return res.redirect('/edit-profile');
+    });
+  });
 });
 
 
