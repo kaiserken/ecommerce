7bf25ff578a2cb3733865dac7b4fa3a1a9cf050b
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-07 15:39:36 -0800

    adding async

--- api/api.js
+++ api/api.js
@@ -3,3 +3,11 @@ var async  = require('async');
 var faker  = require('faker');
 var Category  = require('../models/category');
 var Product  = require('../models/product');
+
+router.get('./:name', function(req, res, next){
+
+  async.waterfall([
+
+  ]);
+
+});
