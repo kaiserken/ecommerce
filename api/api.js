c65e54b8ebccc19a5db090b45ac0588e0ef1c400
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-08 16:07:02 -0800

    added wilcard routes

--- api/api.js
+++ api/api.js
@@ -4,10 +4,29 @@ var faker  = require('faker');
 var Category  = require('../models/category');
 var Product  = require('../models/product');
 
-router.get('./:name', function(req, res, next){
-
+router.get('/:name', function(req, res, next){
+  console.log('in router before waterfall');
   async.waterfall([
-
+    function(callback) {
+      Category.findOne({name: req.params.name }, function(err, category){
+        if (err) return next(err);
+        callback(null, category);
+      });
+    },
+    function(category, callback){
+      for (var i = 0; i < 30; i++){
+        var product  = new Product();
+        product.category = category._id;
+        product.name  = faker.commerce.productName();
+        product.price = faker.commerce.price();
+        product.image = faker.image.image();
+        product.save();
+      }
+    }
   ]);
 
+  res.json({message: 'Success'});
+
 });
+
+module.exports  = router;
