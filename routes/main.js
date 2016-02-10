acf070a482f59e29026899837577c5efff3d8087
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-02-09 16:57:19 -0800

    added elasticsearch

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,52 @@
 var router  = require('express').Router();
 var Product  = require('../models/product');
 
+Product.createMapping(function(err, mapping){
+  if (err){
+    console.log('error created mapping');
+    console.log(err);
+  }else{
+    console.log('mapping created');
+    console.log(mapping);
+  }
+});
+
+var stream  = Product.synchronize();
+var count  = 0;
+
+stream.on('data', function(){
+  count++;
+});
+
+stream.on('close', function(){
+  console.log('Indexed ' + count + ' documents');
+});
+
+stream.on('error', function(err){
+  console.log(err);
+});
+
+router.post('/search', function(req, res, next){
+  res.redirect('/search?q=' + req.body.q);
+});
+
+router.get('/search', function(req, res, next){
+  if (req.query.q){
+    Product.search({
+      query_string : {query: req.query.q}
+    }, function(err, results){
+      if (err) return next(err);
+      var data  = results.hits.hits.map(function(hit){
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
+});
+
 
 router.get('/', function(reg, res){
   res.render('main/home');
